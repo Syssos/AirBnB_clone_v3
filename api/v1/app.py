a9af2c7a10884e5055a228b6ab10aed39c04dace
arturadamian <arturadamian@gmail.com>
2018-08-23 22:52:26 +0000

    add CORS class

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,11 +3,13 @@
 from flask import Flask, Blueprint, jsonify
 from api.v1.views import app_views
 from models import storage
+from flask_cors import CORS
 import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
@@ -23,6 +25,7 @@ def errorhandler(error):
     """ Returns status 404 Not found
     """
     return jsonify({'error': 'Not found'}), 404
+
 
 if __name__ == "__main__":
     hosts = os.getenv("HBNB_API_HOST", default="0.0.0.0")
